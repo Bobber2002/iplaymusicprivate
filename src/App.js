a061ae337f24d8a120a2e8168afbc38e951cb868
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-05-09 21:07:04 +0200

    create onboarding component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Form from "./components/form/Form";
 import { Route, Routes } from "react-router-dom";
+import Onboarding1 from "./components/onboarding/Onboarding1";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App h-full">
       <Routes>
         <Route path="/" element={<Form />} />
+        <Route path="/onboarding-1" element={<Onboarding1 />} />
       </Routes>
     </div>
   );
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,14 +2,16 @@ import React from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { IoIosFingerPrint } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 import "./Form.css";
 const Form = () => {
+  const navigate = useNavigate();
   return (
     <div className="login-form-page h-full pl-6 pr-6 pt-8 pb-8">
       <div className="login-header ">
         <h2 className="text-3xl">Log In</h2>
       </div>
-      <form className="login-form" action="">
+      <div className="login-form" action="">
         <div className="form-group mb-6">
           <label className="font-bold pl-2 pb-3 block">Username</label>
           <div className=" flex justify-center items-center border-b-4 border-black text-xl">
@@ -34,12 +36,22 @@ const Form = () => {
             <RiLockPasswordLine />
           </div>
         </div>
-        <button className="login-button w-full border-4 border-black rounded-3xl p-2">
+        <button
+          onClick={() => {
+            navigate("/onboarding-1");
+          }}
+          className="login-button w-full border-4 border-black rounded-3xl p-2"
+        >
           LOG IN
         </button>
-      </form>
+      </div>
       <div className="login-touch flex pt-8 flex-col items-center">
-        <button className="text-4xl bg-pink-500 w-16 h-16 text-white flex justify-center items-center rounded-full">
+        <button
+          onClick={() => {
+            navigate("/onboarding-1");
+          }}
+          className="text-4xl bg-pink-500 w-16 h-16 text-white flex justify-center items-center rounded-full"
+        >
           <IoIosFingerPrint />
         </button>
         <p className="mt-2 text-gray-500 text-sm">One-Touch Login</p>
