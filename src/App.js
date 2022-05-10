bec5e9366236ebda590be3520d1f56e40ce8e0d8
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-05-10 09:20:32 +0200

    comment out onbording

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Form from "./components/form/Form";
 import { Route, Routes } from "react-router-dom";
-import Onboarding1 from "./components/onboarding/Onboarding1";
+/* import Onboarding1 from "./components/onboarding/Onboarding1";
 import Onboarding2 from "./components/onboarding/Onboarding2";
-import Onboarding3 from "./components/onboarding/Onboarding3";
+import Onboarding3 from "./components/onboarding/Onboarding3"; */
 import "./App.css";
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <div className="App h-full">
       <Routes>
         <Route path="/" element={<Form />} />
-        <Route path="/onboarding-1" element={<Onboarding1 />} />
+        {/* <Route path="/onboarding-1" element={<Onboarding1 />} />
         <Route path="/onboarding-2" element={<Onboarding2 />} />
-        <Route path="/onboarding-3" element={<Onboarding3 />} />
+        <Route path="/onboarding-3" element={<Onboarding3 />} /> */}
       </Routes>
     </div>
   );
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,8 +4,10 @@ import { RiLockPasswordLine } from "react-icons/ri";
 import { IoIosFingerPrint } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import "./Form.css";
+
 const Form = () => {
   const navigate = useNavigate();
+
   return (
     <div className="login-form-page h-full pl-6 pr-6 pt-8 pb-8">
       <div className="login-header ">
