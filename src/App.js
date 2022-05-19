f3363a64382f9d4365585490b7ed8b895f4f86da
vagnqvistgaard <77662595+vagnqvistgaard@users.noreply.github.com>
2022-05-19 12:57:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="/allsongs" element={<Allsongs />} />
         <Route path="/playlist" element={<PlayList />} />
         <Route path="/allartists" element={<AllArtist />} />
+        <Route path="/audioplayer" element={<AudioPlayer />} />  
       </Routes>
     </div>
   );
